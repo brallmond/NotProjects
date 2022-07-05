9b45bed7257a74c62da6773db7cbb9da6be92b39
brallmond <braden.allmond@gmail.com>
2022-07-05 11:51:40 +0200

    threeman file reading changes and a finished project using imshow plotting and real data

---
+++ FinishedProjectsCopy/VariableScanImShow/variableScanImShow.py
@@ -0,0 +1,91 @@
+# Braden Allmond - July 2nd, 2022
+# Testing plotting approach for a scan of three variables on 
+# data consisting of four entries.
+
+# Including useful math and plotting packages
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Including function to get data
+# from this guide
+# https://stackoverflow.com/questions/2349991/how-can-i-import-other-python-files
+from data.realData import getData
+
+# Initializing variable space, like whole numbers
+w = np.linspace(10, 14, 3) # 10, 12, 14
+x = np.linspace(30, 50, 11) 
+y = np.linspace(300, 500, 11)
+
+# Getting real data
+big_foo = getData()
+
+
+# Initiate grid to be filled with counts
+zz = np.zeros((3,11,11))
+
+# Iterate over all three variables in nested for-loops
+# use enumerate to make index:value pairs so we can
+# compare to a value and use that value's index at the same time
+for i, wentry in enumerate(w):
+  for j, xentry in enumerate(x):
+    for k, yentry in enumerate(y):
+      # Iterate over the data
+      for l in range(len(big_foo)):
+        roo = big_foo[l]
+        if ((roo[3] > wentry) and (roo[0] > xentry and roo[1] > xentry) and (roo[2] > yentry)):
+          #print(wentry, xentry, yentry)
+          zz[i,j,k] += 1
+
+# zz is now filled with counts (i.e. if the data satisfies the condition of a zz-cell, increment that cell's value)
+#print(zz)
+
+# plotting from
+# https://stackoverflow.com/questions/13784201/how-to-have-one-colorbar-for-all-subplots
+# and
+# https://stackoverflow.com/questions/41793931/plotting-images-side-by-side-using-matplotlib
+# and
+# https://stackoverflow.com/questions/9295026/matplotlib-plots-removing-axis-legends-and-white-spaces
+
+# Make copies of x and y linspaces but as integers (for plotting)
+intx = [int(i) for i in x]
+inty = [int(i) for i in y]
+
+# Find the maximum grid element and add a small amount, use this value as vmax when plotting
+gridmax = np.max(zz) + 2
+
+# Make 3 subplots in a horizontal line
+fig, axes = plt.subplots(1, 3)
+
+for i, ax in enumerate(axes.flat):
+  # origin='lower' makes the output image cartesian instead of the default image format
+  im = ax.imshow(zz[i], vmin=0, vmax=gridmax, cmap = 'copper', interpolation='nearest', origin='lower')
+  ax.set_title("electronPt " + str(2*(i) + 10), fontsize=8)
+  ax.set_xlabel('jetPt')
+  ax.set_ylabel('mjj')
+
+  # Get start and end of x-axis for tick-mark position info
+  # y-axis has same dimension and positions in our case
+  start, end = ax.get_xlim()
+
+  # Set tick locations explicitly and then assign new tick labels for x- and y-axes
+  ax.xaxis.set_ticks(np.arange(start+0.5, end, 1))
+  ax.xaxis.set_ticklabels(intx)
+  ax.yaxis.set_ticks(np.arange(start+0.5, end, 1))
+  ax.yaxis.set_ticklabels(inty)
+
+  # Put the numerical value of the grid in the grid location
+  # this removes the need for a colorbar (which was too difficult for me to format)
+  for (n,m),label in np.ndenumerate(zz[i]):
+    ax.text(m,n,int(label), ha='center', va='center', color='white', fontsize=6)
+
+  # Only let the y-axis on the first (leftmost) plot be visible
+  if (i != 0):
+    ax.get_yaxis().set_visible(False)
+
+print("If it looks bad, try going full-screen")
+plt.show()
+
+
+
+
+
---
+++ WIP/ThreeMan/three_man.py
@@ -0,0 +1,158 @@
+from random import randint
+from time import sleep
+import numpy as np
+
+# note that this version of threeman is different from what you'll find via google
+# https://eus.wiki/3_Man (top result)
+# this version of threeman I learned in Manhattan, Kansas, in 2020
+#
+# the goal of this program is to determine if there is a relationship
+# between the number of players and the amount each player position drinks
+# secondarily, to determine how likely one player is to be assigned many more
+# drinks than another (via retaining the threeman role)
+#
+# remember arrays start at zero
+
+def increase_player_turn(player_turn):
+  if player_turn == players - 1:
+    player_turn = 0
+  else:
+    player_turn += 1
+  return player_turn
+
+def player_drinks(player_turn, direction): # as in "player that drinks"
+  player_drinks = 0
+  if direction == 1:
+    if player_turn == players - 1:
+      player_drinks = 0
+    else:
+      player_drinks = player_turn + 1
+
+  else:
+    if player_turn == 0:
+      player_drinks = players - 1
+    else:
+      player_drinks = player_turn - 1
+
+  return player_drinks
+
+def exclusive_random(three_man):
+  #select a player to be three_man that isn't the current player
+  exclude = [three_man]
+  new_three_man = randint(0, players - 1)
+  return exclusive_random(three_man) if new_three_man in exclude else new_three_man
+
+
+def updateBuffer(arrayOfStrings):
+    #update output terminal info in place
+    arrayLength = len(arrayOfStrings)
+    for i in range(arrayLength):
+      print(arrayOfStrings[i])
+    sleep(0.0001)
+    bufferControl = '\x1b'+'['+str(arrayLength) +'A' +'\x1b[J'
+    print(bufferControl, end="")
+
+def player_display(player_array, player_turn, three_man):
+    #print player drink counts with player turn and threeman colored in
+    outString = ""
+    for i in range(len(player_array)):
+      if i == player_turn:
+        outString += "\x1b[0;32m" + str(player_array[i]) + "\x1b[0;37m" + " " 
+      elif i == three_man:
+        outString += "\x1b[0;33m" + str(player_array[i]) + "\x1b[0;37m" + " "
+      else:
+        outString += str(player_array[i]) + " " 
+    
+    return outString
+
+def play_full_game(three_man, player_array, player_turn, players, rounds, interactiveDisplay):
+  while rounds > 0:
+    # roll all the dice and find their sum
+    # action = False means no interesting number was rolled
+    # if action stays false after the roll, then it becomes
+    # the next player's turn
+    action = False
+    die_one = randint(1,6)
+    die_two = randint(1,6)
+    die_sum = die_one + die_two
+
+    # if a three is rolled, three man drinks
+    # "drink" = player postiion in player array is incremented
+    # if a three is rolled by the three man, a new three man is selected
+    # (cannot be current three man)
+    if die_one == 3 or die_two == 3 or die_sum%3 == 0:
+      action = True
+      if three_man == player_turn:
+        three_man = exclusive_random(three_man)
+      player_array[three_man] += 1
+      whodrinks = "threeman"
+
+    # if the sum of the dice is seven, the player left
+    # of the three man drinks
+    if die_sum == 7:
+      action = True
+      player_array[player_drinks(player_turn, -1)] += 1
+      whodrinks = "left of player"
+
+    # if the sum of the dice is eleven, the player right
+    # of the three man drinks
+    if die_sum == 11:
+      action = True
+      player_array[player_drinks(player_turn, 1)] += 1
+      whodrinks = "right of player"
+
+    # no fun numbers were rolled, so next turn.
+    # Since this is only incremented when nothing fun is rolled,
+    # it stays one person's turn until they don't roll anything
+    if action == False:
+      player_turn = increase_player_turn(player_turn)
+      whodrinks = "no one"
+    
+    # end of round, print results if interactiveDisplay enabled
+    rounds -= 1
+
+    # interactive display
+    if (interactiveDisplay):
+      printBuffer=[]
+      printBuffer.append("##############")
+      printBuffer.append("ROUND: " + str(rounds))
+      printBuffer.append("dice: " + str(die_one) + " + " + str(die_two) + " = " + str(die_sum))
+      printBuffer.append("action: " + str(action) + '\t' + whodrinks + " drinks")
+      printBuffer.append("player display: " + player_display(player_array, player_turn, three_man))
+      printBuffer.append("##############")
+    
+      updateBuffer(printBuffer)
+
+
+if __name__ == "__main__":
+  # initial setup. choose three man randomly and choose
+  # who starts. Also initialize players to zero drinks
+
+  DataSetSize = 10
+  array_of_arrays = []
+  for i in range(DataSetSize):
+    players = 8
+
+    three_man = randint(0,players-1)
+    player_array = [0 for i in range(players)]
+    player_turn = randint(0,players-1)
+
+    rounds = 100
+    interactiveDisplay = False
+
+    play_full_game(three_man, player_array, player_turn, players, rounds, interactiveDisplay)
+    array_of_arrays.append(player_array)
+
+
+
+# write final results to file
+# if log file, append to it
+# if no log file, make it and write to it
+outputfile = open('output.txt', 'a')
+# strip player array, i.e. transform it from an array into a string separated by commas (?)
+outputfile.write(str(player_array) + '\n')
+
+outputFileName = 'players' + str(players) + 'rounds' + str(rounds) + 'OriginalRules.npy'
+np.save(outputFileName, array_of_arrays)
+
+
