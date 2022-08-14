aca84a494c79ce65d21a6c59d144f4ba99ee52a7
brallmond <braden.allmond@gmail.com>
2022-08-14 16:28:42 +0200

    finished grid project by combining interactive and gif-generating versions of the same code

---
+++ WorksInProgress/Grid/README.txt
@@ -0,0 +1,14 @@
+grid.py
+execute like this (not compatible with python2)
+python3 grid.py
+
+When executed grid.py does two things
+1) it interactively generates a 2D plot (animates the plotting)
+2) it saves a gif of this animated plotting
+
+the main body shows the for loop structure necessary to animate and save as gif
+image.io is a really neat utility!
+
+the function that defines the movement can really be anything 
+that you can put in a function, so this code can be ported easily and used
+for other projects
--- WorksInProgress/Grid/grid.py
+++ WorksInProgress/Grid/grid.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#import random as rand
+import os
+import imageio
 
 # using this site
 # https://www.geeksforgeeks.org/plot-2-d-histogram-in-python-using-matplotlib/
+
 # used this stackoverflow for updating originally
 # https://stackoverflow.com/questions/4098131/how-to-update-a-plot-in-matplotlib
+
 # look at this site for saving a gif
 # https://towardsdatascience.com/basics-of-gifs-with-pythons-matplotlib-54dd544b6f30
 
-# in principle, the "move" function can be modified for different interesting cases/behaviors
-# the main parts of this script are interacting with the plotting tools correctly, but once it's
-# set it's more or less brainless
 
 def move(xin, yin):
 #  for i in range(len(xin)):
@@ -28,17 +28,21 @@ def move(xin, yin):
 
 if __name__ == "__main__":
 
-  # make plot interface and random data
+  # make plot interface and random data using np
   fig = plt.subplots(figsize =(6, 5))
   n = 100
   x = np.random.uniform(-1, 1, size=n)  
   y = np.random.uniform(-1, 1, size=n)
   
   plt.ion() # turn on interactive plotting
-  # make plot, draw it, pause, clear from figure, update data, and restart loop
-  for _ in range(50):
+  # make plot, draw it, make file, save plot to file, pause, clear from figure, update data, and restart loop
+  filenames = []
+  for i in range(50):
     plt.hist2d(x, y, bins=100, range=[[-1,1],[-1,1]])
     plt.draw()
+    filename = f'frame_{i}.png'
+    filenames.append(filename)
+    plt.savefig(filename)
     plt.pause(0.00001)
     plt.clf()
     x, y = move(x, y) # move can be arbitrarily modified for interesting behaviors
@@ -48,4 +52,12 @@ if __name__ == "__main__":
   plt.ioff()
   plt.show()
 
-  
+  # Stich image files together to make a gif, remove the individual file after it's been stitched
+  with imageio.get_writer('output.gif', mode='I') as writer:
+    for filename in filenames:
+      image = imageio.imread(filename)
+      writer.append_data(image)
+      os.remove(filename)
+
+
+
