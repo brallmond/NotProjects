7d1378ec6c9ee9fc03e50dbbf387c42062e07861
Braden Allmond <ballmond@bradens-laptop.home>
2023-01-14 14:27:52 +0100

    addition of bash_profile for mac and changes to gamify_math that slim the program and allow for conversion from hex or binary to decimal

--- FinishedProjects/Small/gamify_math.py
+++ FinishedProjects/Small/gamify_math.py
@@ -2,7 +2,7 @@ from random import randint, choice
 
 # A small terminal-based tool to practice some math
 # namely, converting between C and F and
-# converting binary to decimal
+# converting hex and binary to decimal, and vice versa
 
 def convert_temperature(input_temperature: float, scale_name: str) -> tuple:
   '''
@@ -10,7 +10,11 @@ def convert_temperature(input_temperature: float, scale_name: str) -> tuple:
   Assuming the only scales used are Fahrenheit or Celsius.
 
   input_temperature - the given temperature value to be converted
-  scale - the name of the temperature scale of the given value (expect Fahrenheit or Celsius)
+  scale_name - the name of the temperature scale of the given value (expect Fahrenheit or Celsius)
+
+  Output - tuple:
+  output_temperature - the converted temperature value
+  scale_name - the name of the temperature scale of the output_temperature value
   ''' 
   scale_name = scale_name.lower()
   if ("c" in scale_name):
@@ -29,6 +33,52 @@ def convert_temperature(input_temperature: float, scale_name: str) -> tuple:
   return (output_temperature, scale_name)
 
 
+def convert_number_hex_bin_dec(input_number: int, conversion: str) -> tuple:
+  '''
+  Define and answer the conversion for the user to compare their answer to.
+
+  Example: input_number = 255, conversion = "xd",
+    tell the user "convert 0xff to decimal?"
+           input_number = 4, conversion = "db",
+    ask the user "what is 0d4 in binary?"
+
+  input_number - integer value that will either be converted to bin/hex or passed through the function
+  conversion - string characters "dx", "xd", "db", "bd", indicating the type of conversion
+                       "dx" - convert decimal to hex
+                       "bd" - convert binary to decimal
+                       etc.
+  Output - tuple:
+  value_to_convert - a string the user is asked to convert
+  converted_value - a string of the correct converted value
+  converted_base - a string indicating the base of the converted_value
+  '''
+  input_as_hex = hex(input_number) #type: str
+  input_as_bin = bin(input_number) #type: str
+  input_as_dec = "0d" + str(input_number) 
+
+  if conversion == "dx":
+    value_to_convert = input_as_dec
+    converted_value =  input_as_hex
+    converted_base = "hexadecimal"
+
+  elif conversion == "db":
+    value_to_convert = input_as_dec
+    converted_value =  input_as_bin
+    converted_base = "binary"
+
+  elif conversion == "xd":
+    value_to_convert = input_as_hex
+    converted_value =  input_as_dec
+    converted_base = "decimal"
+
+  elif conversion == "bd":
+    value_to_convert = input_as_bin
+    converted_value  = input_as_dec
+    converted_base = "decimal"
+
+  return (value_to_convert, converted_value, converted_base)
+
+
 def compare_values(correct_answer: str, user_answer: str, exact: bool=False, tolerance: float=0.10) -> str:
   '''
   Compare the correct answer and user answer and determine if they are the same.
@@ -37,7 +87,7 @@ def compare_values(correct_answer: str, user_answer: str, exact: bool=False, tol
   user_answer - supplied from user input
   exact - require answers to match exactly, default behavior is to accept answers within 5%
   '''
-  if exact and (correct_answer == user_answer):
+  if exact and ((user_answer == correct_answer) or (user_answer == correct_answer[2:])):
     print(f"{user_answer} is correct!")
     return True
 
@@ -57,6 +107,9 @@ if __name__ == "__main__":
 
   correct_tally = 0
   incorrect_tally = 0
+  available_mode = "b" in mode or "h" in mode or "t" in mode
+  if not available_mode:
+    print("Please restart the program and select a valid mode.")
   while True:
 
     # (re)initialize
@@ -64,42 +117,39 @@ if __name__ == "__main__":
 
     # temperature conversion mode
     if "t" in mode: 
+      exact_flag = False
       number_to_convert = randint(-100, 451) # Bounds of interesting conversions (bounds from Fahrenheit scale)
-      scale_to_convert = choice([" F", " C"])       # randomly choose either Fahrenheit or Celsius scale
+      scale_to_convert = choice(["F", "C"])       # randomly choose either Fahrenheit or Celsius scale
       correct_value, scale_name = convert_temperature(number_to_convert, scale_to_convert)
-      user_value = input(f"Convert {number_to_convert}{scale_to_convert} : ")
+      user_value = input(f"Convert {number_to_convert} {scale_to_convert} : ")
 
-    # binary mode
-    if "b" in mode:
-      number_to_convert = randint(0, 65) # don't care about working on negative numbers for now
-      correct_value = bin(number_to_convert)
-      user_value = input(f"Convert 0d{number_to_convert} : ")
-
-    # hex mode
-    if "h" in mode:
-      number_to_convert = randint(0, 65)
-      correct_value = hex(number_to_convert)
-      user_value = input(f"Convert 0d{number_to_convert} : ")
-
-    # quit if user says to
-    if "q" in user_value: break
-
-    # otherwise, check answer
+    # hex and binary conversion modes
     if ("b" in mode or "h" in mode):
-      correct = compare_values(str(correct_value), user_value, exact=True)
-    else:
-      correct = compare_values(str(correct_value), user_value)
+      exact_flag = True
+      number_to_convert = randint(0, 256) # don't care about working on negative numbers for now
+      if "h" in mode:
+        conversion = choice(["dx", "xd"])
+      if "b" in mode:
+        conversion = choice(["db", "bd"])
+      value_to_convert, correct_value, converted_base = convert_number_hex_bin_dec(number_to_convert, conversion)
+      user_value = input(f"Convert {value_to_convert} to {converted_base} : ")
+      print(user_value[2:])
+
+    if "q" in user_value: break
+    correct = compare_values(str(correct_value), user_value, exact_flag)
 
     if correct: correct_tally += 1
     else: incorrect_tally += 1
 
-  # something to give a score at the end
-  print("#"*20)
-  print("Here are your stats!")
-  print(f"Correct: {correct_tally}")
-  print(f"Incorrect: {incorrect_tally}")
-  print(f"Percentage: {correct_tally*100/(correct_tally+incorrect_tally)}%")
-  print("#"*20)
+  # give a score at the end
+  if available_mode:
+    print("#"*20)
+    print("Here are your stats!")
+    print(f"Correct: {correct_tally}")
+    print(f"Incorrect: {incorrect_tally}")
+    if ((correct_tally + incorrect_tally) > 0):
+     print(f"Percentage: {correct_tally*100/(correct_tally+incorrect_tally)}%")
+    print("#"*20)
 
 
 
